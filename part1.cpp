9c9e58a7e9ab2cfc1001d4e80884d8b4623063dc
Alex Mattoni <43799350+alexmattoni@users.noreply.github.com>
2024-03-10 11:04:24 -0400

    working on it
    - added more comments
    - matched print text to pdf
    - working on making the random algorithm give the same output as in the pdf

--- part1.cpp
+++ part1.cpp
@@ -7,7 +7,7 @@
 #include <algorithm>
 using namespace std;
 
-vector<char> alphabets = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+char alphabets[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
 struct Process
 {
@@ -34,42 +34,52 @@ int main(int argc, char *argv[])
 
     if (argc < 6)
     {
-        cerr << "Not enough arguments" << endl;
+        cerr << "ERROR: Not enough arguments" << endl;
+        return -1;
     }
 
+    // n = number of processes to simulate
     int num_processes = atoi(argv[1]);
-    int num_cpu = atoi(argv[2]); // I/O or CPU bound
+
+    // n_cpu = number of processes that are CPU-bound
+    int num_cpu = atoi(argv[2]);
+
+    // seed = seed for pseudo RNG to determine interarrival times of burts
     long seed = atol(argv[3]);
+
+    // lambda = exponential distribution parameter
     double lambda = atof(argv[4]);
+
+    // upper_bound = upper bound for valid generated numbers
     int upper_bound = atoi(argv[5]);
 
     srand48(seed);
-
     vector<Process> processes;
 
+    // algorithm to create processes
     for (int i = 0; i < num_processes; ++i)
     {
         Process p;
-        char pid = alphabets[i];
-        p.id = pid;
 
-        int arrivalTime = floor(next_exp(lambda, upper_bound));
-        p.arrivalTime = arrivalTime;
-        
+        // initial parameters
+        p.id = alphabets[i];
+        p.arrivalTime = floor(next_exp(lambda, upper_bound));
         p.numCpuBursts = ceil(drand48() * 64);
-        // cout << p.numCpuBursts << endl;
 
+        // burst generation per process
         for (int j = 0; j < p.numCpuBursts; ++j)
         {
             int cpuBurstTime = (ceil(next_exp(lambda, upper_bound)));
             int ioBurstTime = (ceil(next_exp(lambda, upper_bound))) * 10;
+
+            if (i > num_cpu)
+            {
+                cpuBurstTime *= 4;
+                ioBurstTime /= 8;
+            }
+
             if (j != p.numCpuBursts - 1)
             {
-                if (j < num_cpu)
-                {
-                    cpuBurstTime *= 4;
-                    ioBurstTime /= 8;
-                }
                 p.cpuBurstTime.push_back(cpuBurstTime);
                 p.ioBurstTime.push_back(ioBurstTime);
             }
@@ -81,15 +91,22 @@ int main(int argc, char *argv[])
         processes.push_back(p);
     }
 
-    for (const Process &p : processes)
+
+    // output information for each process
+    std::cout << "<<< PROJECT PART I -- process set (n=" << num_processes << ") with " << num_cpu << " CPU-bound process >>>" << std::endl;
+    for (int i = 0; i < num_processes; i++)
     {
-        std::cout << "Process " << p.id << " - Arrival Time: " << p.arrivalTime << "ms; Num CPU Bursts: " << p.numCpuBursts << "\n";
-        for (int i = 0; i < p.numCpuBursts; ++i)
+        if(i <= num_cpu)
+            std::cout << "I/O-bound ";
+        else
+            std::cout << "CPU-bound ";
+        std::cout << "process " << processes[i].id << ": arrival time " << processes[i].arrivalTime << "ms; " << processes[i].numCpuBursts << " CPU bursts:" << "\n";
+        for (int j = 0; j < processes[i].numCpuBursts; j++)
         {
-            std::cout << "--> CPU burst " << p.cpuBurstTime[i] << "ms";
-            if (i != p.numCpuBursts - 1)
+            std::cout << "--> CPU burst " << processes[i].cpuBurstTime[j] << "ms";
+            if (j != processes[i].numCpuBursts - 1)
             {
-                std::cout << " --> I/O burst " << p.ioBurstTime[i] << "ms";
+                std::cout << " --> I/O burst " << processes[i].ioBurstTime[j] << "ms";
             }
             std::cout << "\n";
         }
